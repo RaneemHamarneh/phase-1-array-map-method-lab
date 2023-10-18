7693d357bc84ca4bca8fef711965a39077328549
raneemhamarneh <raneem99hamarneh@gmail.com>
2023-10-18 13:44:34 +0300

    complete assignment

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (intoNewTitle) =>
+{intoNewTitle = tutorials.map(title => { 
+  return title.split(' ').map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(' ');
+})
+  return intoNewTitle;
 }
+
+console.log(titleCased);
